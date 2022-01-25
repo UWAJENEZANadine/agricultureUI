bbf42513119a2bd8b63f7133330dc56eb75cd568
Usabyimbabazi <Usabyimbabazitarisa@gmail.com>
2022-01-25 02:03:02 -0800

    my first push

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import react from "react"
+import {Routes, Route, useLocation} from "react-router-dom";
+import Home from "../views/home";
+const Index=()=>{
+    const currentUrl=useLocation.pathname
+    return(
+        <Routes>
+            <Route path="/home" element={<Home/>}/>
+        </Routes>
+    )
+}
+export default Index;
---
+++ src/views/home.js
@@ -0,0 +1,7 @@
+import react from "react";
+const Home=()=>{
+    return(
+        <h1>IGIRE MUHINZI</h1>
+    )
+}
+export default Home
