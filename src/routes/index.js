05277586ef6fe5e1a99573cae4870237237e98db
UWAJENEZANadine <adelineu640@gmail.com>
2022-01-27 10:31:01 +0200

    foder cretion of css

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@
 import React from "react"
 import {Routes, Route, useLocation} from "react-router-dom";
 import Home from "../views/home";
+import AboutUs from "../views/aboutUs";
 const Index=()=>{
     const currentUrl=useLocation.pathname
     return(
         <Routes>
             <Route path="/home" element={<Home/>}/>
+            <Route path="/about" element={<AboutUs/>}/>
+
         </Routes>
     )
 }
---
+++ src/views/aboutUs.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HomeLayout from "../components/homeLayout";
+import "../css/aboutUs.css"
+
+const  AboutUs = () =>{
+    return(
+        <HomeLayout>
+            <div className="aboutUs-container" >
+                lkkjlkjlkjlkjlk
+            </div>
+
+
+        </HomeLayout>
+    )
+}
+
+export default AboutUs
