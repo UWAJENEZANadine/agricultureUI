26f3ae3e04dc8835709ee512a1533a7ccd06c9f6
Uwitonze Ange Merveille <uangemerveille@yahoo.com>
2022-01-31 10:15:32 +0200

    adds

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import React from "react"
 import {Routes, Route} from "react-router-dom";
 import Home from "../views/home";
 import AboutUs from "../views/aboutUs";
+import Signin from "../views/signin";
 const Index=()=>{
 //    onst currentUrl=useLocation.pathname c
     return(
         <Routes>
             <Route path="/home" element={<Home/>}/>
             <Route path="/about" element={<AboutUs/>}/>
+            <Route path= "/signin" element={<Signin/>}/>
 
         </Routes>
     )
---
+++ src/views/signin.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomeLayout from "../components/homeLayout"
+import 'antd/dist/antd.css';
+import '../css/signin.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined, FacebookOutlined } from '@ant-design/icons';
+
+const Signin = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+     <HomeLayout>
+         <div className='signin-container'>
+             <h1>Log in here please!</h1>
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <a href="">register now!</a>
+      </Form.Item>
+    </Form>
+    {/* <div className='icons'>
+        <h2>or sign in with</h2>
+    <a href="" style={{width:"30px"}}><FacebookOutlined /></a>
+    </div> */}
+    </div>
+    </HomeLayout>
+  );
+};
+
+export default Signin;
