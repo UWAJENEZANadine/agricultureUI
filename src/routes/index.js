92a9a0d9a782c565d36d765d691c733dec567f26
UWAJENEZANadine <adelineu640@gmail.com>
2022-01-31 09:53:57 +0200

    creating the page of signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 
 import React from "react"
-import {Routes, Route, useLocation} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "../views/home";
 import AboutUs from "../views/aboutUs";
+import SignUp from "../views/SignUp"
 const Index=()=>{
-    const currentUrl=useLocation.pathname
+    
     return(
         <Routes>
             <Route path="/home" element={<Home/>}/>
-            <Route path="/about" element={<AboutUs/>}/>
+            <Route path="/AboutUs" element={<AboutUs/>}/>
+            <Route path="/SignUp" element={<SignUp/>}/>
 
         </Routes>
     )
