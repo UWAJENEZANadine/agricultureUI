aa7fe2be588744cb4023a8f76b83ab56f9df1b38
Iyamuremye Benjamin <yamubbenjamin@gmail.com>
2022-02-15 12:41:48 +0200

    working on feedbacks

--- src/views/aboutUs.js
+++ src/views/aboutUs.js
@@ -10,7 +10,9 @@ import {
 const AboutUs = () => {
   return (
     <HomeLayout>
-      <div className="aboutUs-container"><h1>ABOUT US</h1> </div>
+      <div className="aboutUs-container">
+        <h1>ABOUT US</h1>{" "}
+      </div>
       <div className="about-header">
         {" "}
         <h1>WHY CHOOSE US</h1>
@@ -58,46 +60,22 @@ const AboutUs = () => {
         </div>
       </div>
       <div className="container location-container">
-      <hr className="horizontal-line"></hr>
-
-      {/* <div className="location-container">
+        <hr className="horizontal-line"></hr>
+        {/* <div className="location-container">
         {/* <div className="locationi-container">
           {/* <img src="https://ars.els-cdn.com/content/image/1-s2.0-S2211912418301160-fx1.jpg"  style={{ width:"450px", height:"390px"}}/> */}
-"
-        {/* </div> */} 
-        {/* <div> */} 
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.5443313194805!2d30.073368114104507!3d-1.9345318372208409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca6a5f7eeb027%3A0xec192fd8663d497d!2sRwanda%20National%20Police%20Headquarters!5e0!3m2!1sen!2srw!4v1643276583105!5m2!1sen!2srw"
-            style={{
-              width: "100%",
-              height: "400px",
-              border: "0",
-              allowfullscreen: "yes",
-              loading: "lazy",
-            }}
-          ></iframe>
-
-        <div className="row justify-content-around">
-          <div className="col-10 mt-5 col-sm-5 col-md-5 mb-4  locationi-container">
-            <img
-              src="https://ars.els-cdn.com/content/image/1-s2.0-S2211912418301160-fx1.jpg"
-              style={{ width:"390px",height: "390px" }}
-            />
-            
-          </div>
-          <div className="col-10 mt-5 col-sm-5 col-md-5">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.5443313194805!2d30.073368114104507!3d-1.9345318372208409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca6a5f7eeb027%3A0xec192fd8663d497d!2sRwanda%20National%20Police%20Headquarters!5e0!3m2!1sen!2srw!4v1643276583105!5m2!1sen!2srw"
-              style={{
-                width:"400px",
-                height: "390px",
-                border: "0",
-                allowfullscreen: "yes",
-                loading: "lazy",
-              }}
-            ></iframe>
-          </div>
-        </div>
+        {/* </div> */}
+        {/* <div> */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.5443313194805!2d30.073368114104507!3d-1.9345318372208409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca6a5f7eeb027%3A0xec192fd8663d497d!2sRwanda%20National%20Police%20Headquarters!5e0!3m2!1sen!2srw!4v1643276583105!5m2!1sen!2srw"
+          style={{
+            width: "100%",
+            height: "400px",
+            border: "0",
+            allowfullscreen: "yes",
+            loading: "lazy",
+          }}
+        ></iframe>
       </div>
     </HomeLayout>
   );
